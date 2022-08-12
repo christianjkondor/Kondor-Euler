a8bcdfe432b2280ba5bc73e680fea47dd34b2885
christianjkondor <christianjkondor@gmail.com>
2022-08-12 18:55:39 -0500

    Added comments

--- euler 1.py
+++ euler 1.py
@@ -1,5 +1,18 @@
+# PROJECT EULER
+# Problem 1 | Kondor 2022
+
+# Problem statement:
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
 import math
 
+# The initial function for Euler 1. The basic idea is to find the sum of all multiples of 3, sum of all multiples of 5, then subtract the ones that are double counted, i.e. the multiples of 15.
+
+
+# DOCUMENTATION IN PROGRESS
 def euler1(n):
     threes = (3/2)*math.floor(n/3)*(math.floor(n/3)+1)
     fives = (5/2)*math.floor(n/5)*(math.floor(n/5)+1)
@@ -9,6 +22,7 @@ def euler1(n):
 
 #print(euler1(1000)-1000)
 
+# Currently non-functional. Debugging in progress.
 def GeneralEuler1(n, bases):
     total = 0
 
